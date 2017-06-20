e27e584ed8113a428e7c2eb40da3007e18046ef2
skizor <mr.lidner.l@gmail.com>
2017-06-20 12:37:09 +0600

    Created Pages controller. Added Explore, home, index && profile pages. Changed root url to index

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+#The page controller contains all of the code for pages /pages
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def profile
+  end
+
+  def explore
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+#Define root url
+  root 'pages#index'
+#Define routes for pages
+  get 'home' => 'pages#home'
+  get 'profile' => 'pages#profile'
+  get 'explore' => 'pages#explore'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
